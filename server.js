deb9c87134395137d4f7a39f1ffb2a9e7d5de589
Soheran <108600148+Soheran@users.noreply.github.com>
2024-06-11 15:00:23 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,24 +6,49 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const rooms = {};
+
 io.on('connection', (socket) => {
-    console.log('New client connected');
+  console.log('New client connected');
 
-    socket.on('offer', (data) => {
-        socket.broadcast.emit('offer', data);
-    });
+  socket.on('join-room', ({ roomId, username }) => {
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+    rooms[roomId].push({ id: socket.id, username });
+    socket.join(roomId);
 
-    socket.on('answer', (data) => {
-        socket.broadcast.emit('answer', data);
-    });
+    io.to(roomId).emit('user-joined', { username, users: rooms[roomId].map(user => ({ peerId: user.id, username: user.username })) });
+    console.log(`${username} joined room ${roomId}`);
+  });
 
-    socket.on('ice-candidate', (data) => {
-        socket.broadcast.emit('ice-candidate', data);
-    });
+  socket.on('start-call', ({ roomId }) => {
+    io.to(roomId).emit('user-started-call', { peerId: socket.id, username: socket.username });
+    console.log(`User started call in room ${roomId}`);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+  socket.on('offer', (data) => {
+    socket.to(data.peerId).emit('offer', { offer: data.offer, peerId: socket.id });
+  });
+
+  socket.on('answer', (data) => {
+    socket.to(data.peerId).emit('answer', { answer: data.answer, peerId: socket.id });
+  });
+
+  socket.on('ice-candidate', (data) => {
+    socket.to(data.peerId).emit('ice-candidate', { candidate: data.candidate, peerId: socket.id });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    for (const roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter(user => user.id !== socket.id);
+      io.to(roomId).emit('user-left', { username: socket.username, users: rooms[roomId].map(user => ({ peerId: user.id, username: user.username })) });
+      if (rooms[roomId].length === 0) {
+        delete rooms[roomId];
+      }
+    }
+  });
 });
 
 const PORT = 8080;
